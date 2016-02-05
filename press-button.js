88f5b049b565aba1e347eb776fcea9e5092bd92d
Kendell <kendell@jct.io>
2016-02-05 11:35:07 -0500

    Added button example

---
+++ press-button.js
@@ -0,0 +1,36 @@
+var five = require("johnny-five");
+var board = new five.Board();
+
+board.on("ready", function() {
+  var button   = new five.Button(2);
+  var ledOne   = new five.Led(10);
+  var ledTwo   = new five.Led(11);
+  var ledThree = new five.Led(13);
+
+  button.on("hold", function() {
+    ledOne.blink(1000);
+    ledTwo.blink(800);
+    ledThree.blink(600);
+    console.log("Leds are blinking!");
+    setTimeout(function(){
+      ledOne.stop().off();
+      ledTwo.stop().off();
+      ledThree.stop().off();
+      console.log("Leds blinking has stopped!")
+    }, 3000)
+  });
+
+  button.on("press", function() {
+    ledOne.on();
+    ledTwo.on();
+    ledThree.on();
+    console.log( "Leds are on!" );
+  });
+
+  button.on("release", function() {
+    ledOne.off();
+    ledTwo.off();
+    ledThree.off();
+    console.log("Leds are off!");
+  });
+});
