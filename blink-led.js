349de496b0d476d8a473967c29fafc7103d699f1
Kendell <kendell@jct.io>
2016-02-05 09:30:33 -0500

    Changed the way the boards blink

--- blink-led.js
+++ blink-led.js
@@ -1,11 +1,13 @@
-var five = require("johnny-five");
+// The module
+var five  = require("johnny-five");
+
 var board = new five.Board();
 
 board.on("ready", function() {
   var ledOne   = new five.Led(10);
   var ledTwo   = new five.Led(11);
   var ledThree = new five.Led(13);
-  ledOne.blink(1000);
-  ledTwo.blink(1000);
-  ledThree.blink(1000);
+  ledOne.blink(500);
+  ledTwo.blink(600);
+  ledThree.blink(700);
 });
